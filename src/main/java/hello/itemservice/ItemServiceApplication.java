f50412f8d8cd165ee3ffb8329208751444d684e1
Geondol <keedob@naver.com>
2022-09-21 16:24:53 +0900

    Querydsl 공부

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -16,7 +16,8 @@ import javax.sql.DataSource;
 @Slf4j
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
